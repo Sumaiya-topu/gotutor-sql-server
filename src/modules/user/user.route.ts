51ad64ffd73acae54c8f583fcfb9168fac8ac2fe
Sumaiya-topu <topusumaiya@gmail.com>
2023-11-11 15:38:58 +0600

    Data inserted into the user mode successfully

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { UserController } from './user.controller';
 
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.send("hello prisma");
 })
+
+router.post('/create-user', UserController.insertIntoDB)
 
 export const UserRoutes = router;
