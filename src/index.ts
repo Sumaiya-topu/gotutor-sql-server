1a3e45904f29e04d2135ae10a4af63ff8cad1408
Sumaiya-topu <topusumaiya@gmail.com>
2023-11-11 15:22:42 +0600

    model view controller added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Application } from 'express';
-import cors from 'cors';
+import app from "./app";
 
-const app:Application = express();
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
 
 
 const prisma = new PrismaClient();
